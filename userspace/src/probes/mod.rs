f5949f38b6ee2426e936b3f1c72714688becb143
Raffin, Guillaume <guillaume.raffin@atos.net>
2023-05-02 13:31:49 +0200

    new probe design (but unpractical for msr and epbf, so I won't use it)

--- userspace/src/probes/mod.rs
+++ userspace/src/probes/mod.rs
@@ -1,29 +1,85 @@
 pub mod ebpf;
+pub mod msr;
 pub mod perf_rapl;
 pub mod powercap;
-pub mod msr;
 
-pub trait Probe: Send {
-    /// Reads the current value of the probe, in **microJoules**.
-    fn read_uj(&mut self, out: &mut Vec<EnergyMeasurement>) -> anyhow::Result<()>;
+/// An energy counter that can be read.
+pub trait EnergyCounter: Send {
+    /// Returns the current value of the energy counter.
+    /// This method does **not** apply any conversion.
+    fn read(&self) -> anyhow::Result<u64>;
 }
 
-#[derive(Debug)]
-pub struct EnergyMeasurement {
-    /// The energy **counter**, in microjoules.
-    /// This counter is modified by RAPL, and can overflow.
-    /// If you need the energy consumed by a task,
-    /// you need to measure before and after the task:
+pub struct EnergyProbe<C> {
+    counter: C,
+    previous_value: Option<u64>,
+    energy_unit: f64,
+}
+
+impl<C: EnergyCounter> EnergyProbe<C> {
+    /// Creates a new `EnergyProbe`.
+    ///
+    /// `counter_value * energy_unit = Joules`
+    pub fn new(counter: C, energy_unit: f64) -> EnergyProbe<C> {
+        EnergyProbe {
+            counter,
+            previous_value: None,
+            energy_unit,
+        }
+    }
+
+    /// Returns the number of Joules consumed since the last call,
+    /// based on the underlying energy counter, and a flag that indicates
+    /// whether an overflow has occured (`true` means overflow, `false` means no overflow).
+    ///
+    /// If this is the first call, returns `None`.
+    ///
+    /// ## Usage
     ///
     /// ```ignore
-    /// let before = probe.read_uj();
-    /// run_task();
-    /// let after = probe.read_uj();
-    /// let consumed_energy = after.energy_counter - before.energy_counter;
-    /// println!("Task consumed {consumed_energy} microJoules");
+    /// if let Some((joules, overflow)) = probe.read_consumed_joules() {
+    ///     println!("{joules} J consumed");
+    /// }
+    ///
     /// ```
-    pub energy_counter: u64,
+    ///
+    /// ## Overflows
+    ///
+    /// RAPL counters overflow after some time, which depends on the consumption
+    /// of the monitored domain. This time can be lower than one minute.
+    /// To avoid losing data and reporting wrong measurements, no more than one overflow
+    /// must occur between two measurements. That is, the polling frequency must be high enough.
+    ///
+    /// If two consecutive calls return `true`, then the frequency is either too low,
+    /// or barely right (but that's risky).
+    ///
+    pub fn read_consumed_joules(&self) -> Option<(f64, bool)> {
+        let current = self.counter.read();
+        let res = {
+            if let Some(prev) = self.previous_value {
+                if current < prev {
+                    // one or more overflow have occured, we cannot know how many,
+                    // so we assume it was only one.
+                    let corrected = u64::MAX - prev + current;
+                    Some((corrected * self.energy_unit, true))
+                } else {
+                    let diff = current - prev;
+                    Some((diff * self.energy_unit, false))
+                }
+            } else {
+                None
+            }
+        };
+        self.previous_value = Some(current);
+        res
+    }
 
-    /// The logical cpu number (0-N where N is the number of _cores_ on the machine).
-    pub cpu: u32,
+    // NOTE: the energy_counter can be a floating-point number in Joules,
+    // without any loss of precision. Why? Because multiplying any number
+    // by a float that is a power of two will only change the "exponent" part,
+    // not the "mantissa".
+    //
+    // A f32 can hold integers without any precision loss
+    // up to approximately 2^24, which is not enough for the RAPL counter values,
+    // so we use a f64 here.
 }
