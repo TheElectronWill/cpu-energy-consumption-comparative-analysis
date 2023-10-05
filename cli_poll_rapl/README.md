b709c737814dbe3c7254957ee86b9be352d53285
Guillaume Raffin <theelectronwill@gmail.com>
2023-10-05 16:53:14 +0200

    Improve microbenchmark

--- cli_poll_rapl/README.md
+++ cli_poll_rapl/README.md
@@ -6,6 +6,14 @@ Benchmark the functions that read the RAPL counters, with the *criterion* tool.
 
 Results are in [target/criterion/report/index.html](../target/criterion/report/index.html).
 
+### How to run
+
+Running the benchmarks require to execute `cargo bench` **twice**, as follows:
+
+1. Compile and attempt to run with `cargo bench`. It will fail with a panick, because the compiled binary does not have the required linux capabilities and thus cannot access the RAPL counters.
+2. Add capabilities to the binary file: `sudo setcap cap_sys_rawio,cap_sys_admin=ep ../target/x86_64-unknown-linux-musl/release/deps/benchmark_probes-*`
+3. Run the microbenchmark: `cargo bench`
+
 ## CLI benchmark app (src/)
 
 A CLI app that runs a benchmark with `sysbench` + RAPL polling.
--- cli_poll_rapl/benches/benchmark_probes.rs
+++ cli_poll_rapl/benches/benchmark_probes.rs
@@ -12,31 +12,35 @@ use rapl_probes::{
 use rapl_probes::ebpf::EbpfProbe;
 
 fn init_powercap_probe<const CHECK_UTF: bool>(domains: &[RaplDomainType]) -> anyhow::Result<PowercapProbe<CHECK_UTF>> {
-    let cpus = rapl_probes::cpus_to_monitor()?;
+    let cpu = rapl_probes::cpus_to_monitor()?.first().unwrap().clone();
+    let cpus = &[cpu];
     let all = powercap::all_power_zones()?.flat;
-    let zones: Vec<&powercap::PowerZone> = all.iter().filter(|z| domains.contains(&z.domain)).collect();
-    PowercapProbe::new(&cpus, &zones)
+    let zones: Vec<&powercap::PowerZone> = all.iter().filter(|z| domains.contains(&z.domain) && (z.socket_id.is_some_and(|s| cpu.socket == s))).collect();
+    PowercapProbe::new(cpus, &zones)
 }
 
 fn init_perf_probe(domains: &[RaplDomainType]) -> anyhow::Result<PerfEventProbe> {
-    let cpus = rapl_probes::cpus_to_monitor()?;
+    let cpu = rapl_probes::cpus_to_monitor()?.first().unwrap().clone();
+    let cpus = &[cpu];
     let all = perf_event::all_power_events()?;
     let events: Vec<&perf_event::PowerEvent> = all.iter().filter(|e| domains.contains(&e.domain)).collect();
-    PerfEventProbe::new(&cpus, &events)
+    PerfEventProbe::new(cpus, &events)
 }
 
 #[cfg(feature = "bench_ebpf")]
 fn init_ebpf_probe(domains: &[RaplDomainType]) -> anyhow::Result<EbpfProbe> {
     let all = perf_event::all_power_events()?;
-    let cpus = rapl_probes::cpus_to_monitor()?;
+    let cpu = rapl_probes::cpus_to_monitor()?.first().unwrap().clone();
+    let cpus = &[cpu];
     let events: Vec<&perf_event::PowerEvent> = all.iter().filter(|e| domains.contains(&e.domain)).collect();
     let freq_hz = 1000;
-    EbpfProbe::new(&cpus, &events, freq_hz)
+    EbpfProbe::new(cpus, &events, freq_hz)
 }
 
 fn init_msr_probe(domains: &[RaplDomainType]) -> anyhow::Result<MsrProbe> {
-    let cpus = rapl_probes::cpus_to_monitor()?;
-    MsrProbe::new(&cpus, domains)
+    let cpu = rapl_probes::cpus_to_monitor()?.first().unwrap().clone();
+    let cpus = &[cpu];
+    MsrProbe::new(cpus, domains)
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
@@ -47,32 +51,20 @@ fn criterion_benchmark(c: &mut Criterion) {
             "3",
             &[RaplDomainType::Package, RaplDomainType::PP0, RaplDomainType::Platform],
         ),
-        ("5name", &RaplDomainType::ALL),
+        ("5", &RaplDomainType::ALL),
         ("5ordered", &RaplDomainType::ALL_IN_ADDR_ORDER),
     ];
 
     // criterion config
     let mut group = c.benchmark_group("RAPL");
     group
-        .significance_level(0.005)
+        .significance_level(0.01)
         .sample_size(1000)
         .warm_up_time(Duration::from_secs(2))
-        .measurement_time(Duration::from_secs(10));
+        .measurement_time(Duration::from_secs(30));
 
     // benchmark definitions for each list of RAPL domains
     for (id, domains) in domains_lists {
-        let mut probe_powercap = init_powercap_probe::<true>(&domains).unwrap();
-        let mut probe_powercap_unchecked = init_powercap_probe::<false>(&domains).unwrap();
-        let mut probe_perf = init_perf_probe(&domains).unwrap();
-        let mut probe_msr = init_msr_probe(&domains).unwrap();
-
-        // Most of the time spent by the ebpf probe is kernel time, not user time, and it's not measured by criterion.
-        // Therefore, it's disabled by default.
-        #[cfg(feature = "bench_ebpf")]
-        let runtime = tokio::runtime::Runtime::new().unwrap(); // ebpf requires the tokio runtime to asynchronously poll the buffers
-        #[cfg(feature = "bench_ebpf")]
-        let mut probe_ebpf = runtime.block_on(async { init_ebpf_probe(&domains).unwrap() });
-
         // the benchmark
         let mut run_bench = |name: &str, probe: &mut dyn EnergyProbe| {
             let id = BenchmarkId::new(name, id);
@@ -86,12 +78,38 @@ fn criterion_benchmark(c: &mut Criterion) {
         };
 
         // run it
-        run_bench("powercap-sysfs", &mut probe_powercap);
-        run_bench("powercap-sysfs-unchecked", &mut probe_powercap_unchecked);
-        run_bench("perf-event-user", &mut probe_perf);
-        run_bench("msr-lowlevel", &mut probe_msr);
+        {
+            let mut probe_powercap = init_powercap_probe::<true>(&domains).unwrap();
+            run_bench("powercap", &mut probe_powercap);
+        }
+
+        {
+            let mut probe_perf = init_perf_probe(&domains).unwrap();
+            run_bench("perf", &mut probe_perf);
+        }
+
+        {
+            let mut probe_msr = init_msr_probe(&domains).unwrap();
+            run_bench("msr", &mut probe_msr);
+        }
+
+        #[cfg(feature = "bench_powercap_unchecked")]
+        {
+            let mut probe_powercap_unchecked = init_powercap_probe::<false>(&domains).unwrap();
+            run_bench("powercap-unchecked", &mut probe_powercap_unchecked);
+        }
+
         #[cfg(feature = "bench_ebpf")]
-        run_bench("perf-event-ebpf", &mut probe_msr);
+        {
+            // Most of the time spent by the ebpf probe is kernel time, not user time, and it's not measured by criterion.
+            // Therefore, it's disabled by default.
+            #[cfg(feature = "bench_ebpf")]
+            let runtime = tokio::runtime::Runtime::new().unwrap(); // ebpf requires the tokio runtime to asynchronously poll the buffers
+            #[cfg(feature = "bench_ebpf")]
+            let mut probe_ebpf = runtime.block_on(async { init_ebpf_probe(&domains).unwrap() });
+
+            run_bench("ebpf", &mut probe_ebpf);
+        }
     }
 }
 
